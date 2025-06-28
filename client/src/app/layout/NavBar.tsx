35e304f651e1a5efe3dea084dca319a11734ae6c
omerkocadere <okocadere@gmail.com>
2025-06-28 11:17:21 +0300

    refactor(navbar): improve NavBar component structure and styling
    - Reorganized import statements for better readability
    - Enhanced formatting of midLinks and rightLinks arrays
    - Improved navStyles for consistency
    - No change to functionality or logic

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,86 +1,93 @@
-import { AppBar, Badge, Box, IconButton, LinearProgress, List, ListItem, Toolbar, Typography } from "@mui/material";
-import { DarkMode, LightMode, ShoppingCart } from '@mui/icons-material';
+import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  LinearProgress,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { NavLink } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { setDarkMode } from "./uiSlice";
 
 const midLinks = [
-    { title: 'catalog', path: '/catalog' },
-    { title: 'about', path: '/about' },
-    { title: 'contact', path: '/contact' },
-]
+  { title: "catalog", path: "/catalog" },
+  { title: "about", path: "/about" },
+  { title: "contact", path: "/contact" },
+];
 
 const rightLinks = [
-    { title: 'login', path: '/login' },
-    { title: 'register', path: '/register' }
-]
+  { title: "login", path: "/login" },
+  { title: "register", path: "/register" },
+];
 
 const navStyles = {
-    color: 'inherit',
-    typography: 'h6',
-    textDecoration: 'none',
-    '&:hover': {
-        color: 'grey.500'
-    },
-    '&.active': {
-        color: '#baecf9'
-    }
-}
+  color: "inherit",
+  typography: "h6",
+  textDecoration: "none",
+  "&:hover": {
+    color: "gray.500",
+  },
+  "&.active": {
+    color: "#baecf9",
+  },
+};
 
 export default function NavBar() {
-    const {isLoading, darkMode} = useAppSelector(state => state.ui);
-    const dispatch = useAppDispatch();
+  const { isLoading, darkMode } = useAppSelector((state) => state.ui);
+  const dispatch = useAppDispatch();
 
-    return (
-        <AppBar position="fixed">
-            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-                <Box display='flex' alignItems='center'>
-                    <Typography component={NavLink} sx={navStyles} to='/' variant="h6">RE-STORE</Typography>
-                    <IconButton onClick={() => dispatch(setDarkMode())}>
-                        {darkMode ? <DarkMode /> : <LightMode sx={{ color: 'yellow' }} />}
-                    </IconButton>
-                </Box>
+  return (
+    <AppBar position="fixed">
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box display="flex" alignItems="center">
+          <Typography component={NavLink} sx={navStyles} to="/" variant="h6">
+            RE-STORE
+          </Typography>
+          <IconButton onClick={() => dispatch(setDarkMode())}>
+            {darkMode ? <DarkMode /> : <LightMode sx={{ color: "yellow" }} />}
+          </IconButton>
+        </Box>
 
-                <List sx={{ display: 'flex' }}>
-                    {midLinks.map(({ title, path }) => (
-                        <ListItem
-                            component={NavLink}
-                            to={path}
-                            key={path}
-                            sx={navStyles}
-                        >
-                            {title.toUpperCase()}
-                        </ListItem>
-                    ))}
-                </List>
+        <List sx={{ display: "flex" }}>
+          {midLinks.map(({ title, path }) => (
+            <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+              {title.toUpperCase()}
+            </ListItem>
+          ))}
+        </List>
 
-                <Box display='flex' alignItems='center'>
-                    <IconButton size="large" sx={{ color: 'inherit' }}>
-                        <Badge badgeContent='4' color="secondary">
-                            <ShoppingCart />
-                        </Badge>
-                    </IconButton>
+        <Box display="flex" alignItems="center">
+          <IconButton size="large" sx={{ color: "inherit" }}>
+            <Badge badgeContent="4" color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
 
-                    <List sx={{ display: 'flex' }}>
-                        {rightLinks.map(({ title, path }) => (
-                            <ListItem
-                                component={NavLink}
-                                to={path}
-                                key={path}
-                                sx={navStyles}
-                            >
-                                {title.toUpperCase()}
-                            </ListItem>
-                        ))}
-                    </List>
-                </Box>
-
-            </Toolbar>
-            {isLoading && (
-                <Box sx={{width: '100%'}}>
-                    <LinearProgress color="secondary" />
-                </Box>
-            )}
-        </AppBar>
-    )
+          <List sx={{ display: "flex" }}>
+            {rightLinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={navStyles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Toolbar>
+      {isLoading && (
+        <Box sx={{ width: "100%" }}>
+          <LinearProgress color="secondary" />
+        </Box>
+      )}
+    </AppBar>
+  );
 }
--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,17 +1,24 @@
-import { Box } from "@mui/material"
-import { Product } from "../../app/models/product"
-import ProductCard from "./ProductCard"
+import { Box } from "@mui/material";
+import type { Product } from "../../app/models/product";
+import ProductCard from "./ProductCard";
 
 type Props = {
-    products: Product[]
-}
+  products: Product[];
+};
 
-export default function ProductList({products}: Props) {
-    return (
-        <Box sx={{display: 'flex', flexWrap: 'wrap', gap: 3, justifyContent: 'center'}}>
-            {products.map(product => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </Box>
-    )
+export default function ProductList({ products }: Props) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: 3,
+        justifyContent: "center",
+      }}
+    >
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </Box>
+  );
 }
